ee48d2e179f76d13f0237f6dd1f9376b9c01c5e7
NguyenVanThang2505 <nguyenvanthang2505200425@gmail.com>
2025-03-24 16:29:36 +0700

    Delete LAB-01 directory

--- LAB-01/ex02/ex02_02.py
+++
@@ -1,3 +0,0 @@
-bankinh = float(input("Nhập bán kính: "))
-dientich = 3.14 * (bankinh ** 2)
-print("Diện tích của hình tròn là: ", dientich)
--- LAB-01/ex02/ex02_10.py
+++
@@ -1,6 +0,0 @@
-def daoNguocChuoi(chuoi):
-    return chuoi[::-1]
-
-chuoi = input("Nhập chuỗi để đảo ngược: ")
-chuoiDaoNguoc = daoNguocChuoi(chuoi)
-print(f"Chuỗi đảo ngược là: {chuoiDaoNguoc}")
